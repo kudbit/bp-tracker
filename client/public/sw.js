afdccb07df8b04b729c8f877b9a2364c3d83494a
kudbit <yashrajsrma@gmail.com>
2026-04-17 10:18:30 +0530

    Feat: Add Progressive Web App (PWA) manifest and service worker to enable native installation

---
+++ client/public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  // Empty dummy fetch handler specifically to satisfy Chrome's PWA install criteria
+});
